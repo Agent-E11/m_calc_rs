fbf1d1e42e660760cc87dd72eab420e2c28b819a
Eamon <eamon.r.burns@gmail.com>
2023-11-20 23:34:01 -0800

    - Refactored `main.rs` - Changed the main function to have a loop. Now can calculate multiple expressions and keeps context between them

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use std::collections::HashMap;
 use m_calc::{calculate, tokenize};
 
 fn main() {
-
     let mut buf = String::new();
-
-    io::stdin().read_line(&mut buf).unwrap();
-
     let mut context = HashMap::new();
-    let tokens = tokenize(&buf).unwrap();
-    println!("Tokens: {tokens:?}");
 
-    println!("\n--- Calculation ---\nResult:  {:?}\nContext: {:?}", calculate(&tokens, &mut context), context);
+    loop {
+        io::stdin().read_line(&mut buf).unwrap();
+
+        let tokens = tokenize(&buf).unwrap();
+
+        println!("\n--- Calculation ---\nResult:  {:?}\nContext: {:?}", calculate(&tokens, &mut context), context);
+
+        buf.clear();
+    }
 }
