3af3a526a059446b3645efe86eb807be8905db34
Eamon <eamon.r.burns@gmail.com>
2023-11-24 22:02:54 -0800

    - Added loggers - Added `log` and `env_logger` logging packages to dependencies Added `init_logger` function to initialize an `env_logger` with the given settings Replaced all debug `println`s in `tokenize` with `log::debug`s

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
 use std::io;
 use std::collections::HashMap;
+use log::{error, warn, info, debug, trace};
 
-use m_calc::{calculate, tokenize};
+use m_calc::{calculate, tokenize, init_logger, CalcErr};
 
 fn main() {
+    init_logger("RUST_LOG", "");
     let mut buf = String::new();
     let mut context = HashMap::new();
 
     loop {
+        println!("type `/q` to quit");
         io::stdin().read_line(&mut buf).unwrap();
 
-        let tokens = tokenize(&buf).unwrap();
+        buf = buf.trim().to_string();
 
-        println!("\n--- Calculation ---\nResult:  {:?}\nContext: {:?}", calculate(&tokens, &mut context), context);
+        if buf == "/q" {
+            break;
+        }
+
+        let tokens = match tokenize(&buf) {
+            Ok(t) => {
+                info!("successful tokenization: `{t:?}`");
+                t
+            },
+            Err(CalcErr(e)) => {
+                error!("{e:?}");
+                buf.clear();
+                continue;
+            },
+        };
+
+        let res = match calculate(&tokens, &mut context) {
+            Ok(t) => {
+                info!("successful calculation: `{t}`");
+                t
+            },
+            Err(CalcErr(e)) => {
+                error!("{e:?}");
+                buf.clear();
+                continue;
+            },
+        };
+
+        println!("\n--- Calculation ---\nResult:  {:?}\nContext: {:?}", res, context);
 
         buf.clear();
     }
